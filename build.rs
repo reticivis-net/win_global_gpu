4ed74f6d31b22850e627f7003c57242461fac90c
Melody <reticivis@gmail.com>
2023-11-29 09:47:57 -0600

    require admin

--- build.rs
+++ build.rs
@@ -3,8 +3,22 @@ use winres::WindowsResource;
 fn main() -> io::Result<()> {
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
         WindowsResource::new()
-            // This path can be absolute, or relative to your crate root.
+            // set exe icon
             .set_icon("win_global_gpu.ico")
+            // require admin
+            .set_manifest(
+                r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+</trustInfo>
+</assembly>
+"#,
+            )
             .compile()?;
     }
     Ok(())

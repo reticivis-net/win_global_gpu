98842ba36a4094eb7e4a819de3aa31ae8e454586
Melody <reticivis@gmail.com>
2023-12-11 18:44:39 -0600

    tidy up the cli

--- src/hide_console.rs
+++ src/hide_console.rs
@@ -2,6 +2,9 @@ use anyhow::Result;
 use windows::Win32::System::Console::{FreeConsole, GetConsoleProcessList};
 
 pub unsafe fn hide_console() -> Result<()> {
+    // by @interacsion on the Rust discord, not 100% how it works but it works
+    // side effect is that RustRover's debug also gets detached, so this is only run in release mode
+    println!("Releasing console...");
     let mut processes = [0; 2];
     if GetConsoleProcessList(&mut processes) == 1 {
         // free my boy console he aint do nothin wrong
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod charging_events;
 mod elevate;
 mod exe_scan_2;
 mod full_win_scan;
+#[cfg(not(debug_assertions))]
 mod hide_console;
 mod hstring_utils;
 mod notification;
@@ -22,7 +23,10 @@ fn unplug() {
     match res {
         Ok(_) => {
             println!("Wrote to registry!");
-            notification::toast("Set global GPU to integrated GPU.").unwrap();
+            notification::toast(
+                "Set global GPU to integrated GPU.\nRestart programs to see changes.",
+            )
+            .unwrap();
         }
         Err(e) => {
             notification::toast("Error writing to registry.").unwrap();
@@ -35,7 +39,10 @@ fn plug() {
     match res {
         Ok(_) => {
             println!("Wrote to registry!");
-            notification::toast("Set global GPU to dedicated GPU.").unwrap();
+            notification::toast(
+                "Set global GPU to dedicated GPU.\nRestart programs to see changes.",
+            )
+            .unwrap();
         }
         Err(e) => {
             notification::toast("Error writing to registry.").unwrap();
@@ -50,15 +57,16 @@ fn kill_duplicate() -> Result<bool> {
     unsafe { prevent_duplicate::kill_older_process() }
 }
 fn set_programs() -> Result<()> {
-    if PROGRAMS.set(full_win_scan::get_all_programs()?).is_err() {
-        return Err(anyhow!("Failed to store program list."));
-    }
-    Ok(())
+    PROGRAMS
+        .set(full_win_scan::get_all_programs()?)
+        .map_err(|_| anyhow!("Failed to store program list."))
 }
 fn core() -> Result<()> {
     kill_duplicate()?;
     set_programs()?;
     notification::register()?;
+    // only hide console in release mode
+    // RustRover's debug mode counts as an attached console and gets freed
     #[cfg(not(debug_assertions))]
     unsafe {
         hide_console::hide_console()?
@@ -67,22 +75,33 @@ fn core() -> Result<()> {
     Ok(())
 }
 
+fn prog() -> Result<String> {
+    // modified from https://stackoverflow.com/a/58113997/9044183
+    env::current_exe()?
+        .file_name()
+        .ok_or(anyhow!("No file name"))?
+        .to_os_string()
+        .into_string()
+        .map_err(|e| anyhow!("Failed to convert {e:?} to String"))
+}
+
 fn main() -> Result<()> {
-    println!("Hello, world!");
-    elevate::elevate_if_needed()?;
     match env::args().collect::<Vec<String>>().get(1) {
         None => {
+            elevate::elevate_if_needed()?;
             core()?;
         }
         Some(arg) => {
             match arg.as_str() {
                 "shutdown" => {
+                    elevate::elevate_if_needed()?;
                     let older_proc = kill_duplicate()?;
                     if !older_proc {
                         println!("No other instance of Win Global GPU found.")
                     }
                 }
                 "dedicated" => {
+                    elevate::elevate_if_needed()?;
                     set_programs()?;
                     unsafe {
                         registry::write_reg(PROGRAMS.get().unwrap(), registry::GpuMode::Dedicated)?
@@ -90,6 +109,7 @@ fn main() -> Result<()> {
                     println!("Set global GPU to dedicated GPU.");
                 }
                 "integrated" => {
+                    elevate::elevate_if_needed()?;
                     set_programs()?;
                     unsafe {
                         registry::write_reg(PROGRAMS.get().unwrap(), registry::GpuMode::Integrated)?
@@ -97,17 +117,30 @@ fn main() -> Result<()> {
                     println!("Set global GPU to integrated GPU.");
                 }
                 "reset" => {
+                    elevate::elevate_if_needed()?;
                     unsafe { registry::write_reg(&vec![], registry::GpuMode::None)? };
                     println!("Reset!");
                 }
+                // env!("CARGO_PKG_VERSION") is the cargo version
+                // idk why its an env variable but whatever
+                "help" => {
+                    println!(
+                        include_str!("../help.txt"),
+                        env!("CARGO_PKG_VERSION"),
+                        prog()?
+                    )
+                }
+                "about" => {
+                    println!(include_str!("../about.txt"), env!("CARGO_PKG_VERSION"))
+                }
                 err => {
-                    eprintln!("Invalid argument {err}. Run `win_global_gpu.exe help` to see all commands.")
+                    eprintln!(
+                        "Invalid argument `{err}`. Run `{} help` to see all commands.",
+                        prog()?
+                    )
                 }
             }
         }
     }
-
-    // unsafe { registry::write_reg(&vec!(), registry::GpuMode::None)?;}
-    // it doesnt return an error type it returns the stuff already in the var so ? doesn't work
     Ok(())
 }

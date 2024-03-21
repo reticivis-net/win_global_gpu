fc64aa3201932f819643b7940967aff12d354dc7
Melody <reticivis@gmail.com>
2024-03-21 18:39:23 -0500

    rustrover whyyyyy

--- src/notification.rs
+++ src/notification.rs
@@ -4,7 +4,7 @@ use winrt_toast::{Toast, ToastDuration, ToastManager};
 const ICON: &[u8] = include_bytes!("../assets/win_global_gpu.ico");
 pub fn register() -> Result<()> {
     // is this the worst fucking thing ever? absolutely. does it work? yes :tro:
-    let temp = std::env::temp_dir().join("../assets/win_global_gpu.ico");
+    let temp = std::env::temp_dir().join("win_global_gpu.ico");
     // assume if it exists it's probably the one i want
     if !temp.exists() {
         std::fs::write(&temp, ICON)?;

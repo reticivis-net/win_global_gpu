e446edd4dacfe981a3753754e1442ee6ef1a702e
Melody <reticivis@gmail.com>
2024-04-08 18:01:05 -0500

    add optimus option

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod full_win_scan;
 mod hide_console;
 mod hstring_utils;
 mod notification;
+mod optimus;
 mod panic;
 mod prevent_duplicate;
 mod registry;
@@ -51,6 +52,38 @@ fn plug() {
         }
     }
 }
+fn plug_optimus() {
+    let res = unsafe { optimus::dedicated() };
+    match res {
+        Ok(_) => {
+            println!("Changed to dedicated GPU!");
+            notification::toast(
+                "Set global GPU to dedicated GPU.\nRestart programs to see changes.",
+            )
+            .unwrap();
+        }
+        Err(e) => {
+            notification::toast("Error changing to dedicated GPU.").unwrap();
+            dbg!(e);
+        }
+    }
+}
+fn unplug_optimus() {
+    let res = unsafe { optimus::integrated() };
+    match res {
+        Ok(_) => {
+            println!("Changed to integrated GPU!");
+            notification::toast(
+                "Set global GPU to integrated GPU.\nRestart programs to see changes.",
+            )
+            .unwrap();
+        }
+        Err(e) => {
+            notification::toast("Error changing to integrated GPU.").unwrap();
+            dbg!(e);
+        }
+    }
+}
 
 static PROGRAMS: OnceLock<Vec<HSTRING>> = OnceLock::new();
 
@@ -62,17 +95,23 @@ fn set_programs() -> Result<()> {
         .set(full_win_scan::get_all_programs()?)
         .map_err(|_| anyhow!("Failed to store program list."))
 }
-fn core() -> Result<()> {
+fn core(use_optimus: bool) -> Result<()> {
     setup_panic_hook();
     kill_duplicate()?;
-    set_programs()?;
+    if !use_optimus {
+        set_programs()?;
+    }
     notification::register()?;
     // only hide console in release mode
     // RustRover's debug mode counts as an attached console and gets freed
     if cfg!(not(debug_assertions)) {
         unsafe { hide_console::hide_console()? }
     }
-    unsafe { charging_events::register_events(unplug, plug)? }
+    if use_optimus {
+        unsafe { charging_events::register_events(unplug_optimus, plug_optimus)? }
+    } else {
+        unsafe { charging_events::register_events(unplug, plug)? }
+    }
     Ok(())
 }
 
@@ -87,10 +126,12 @@ fn prog() -> Result<String> {
 }
 
 fn main() -> Result<()> {
-    match env::args().collect::<Vec<String>>().get(1) {
+    let mut pargs = pico_args::Arguments::from_env();
+    let use_optimus = pargs.contains(["-o", "--optimus"]);
+    match pargs.subcommand()? {
         None => {
             elevate::elevate_if_needed()?;
-            core()?;
+            core(use_optimus)?;
         }
         Some(arg) => {
             match arg.as_str() {
@@ -105,7 +146,14 @@ fn main() -> Result<()> {
                     elevate::elevate_if_needed()?;
                     set_programs()?;
                     unsafe {
-                        registry::write_reg(PROGRAMS.get().unwrap(), registry::GpuMode::Dedicated)?
+                        if use_optimus {
+                            optimus::dedicated()?;
+                        } else {
+                            registry::write_reg(
+                                PROGRAMS.get().unwrap(),
+                                registry::GpuMode::Dedicated,
+                            )?
+                        }
                     };
                     println!("Set global GPU to dedicated GPU.");
                 }
@@ -113,13 +161,26 @@ fn main() -> Result<()> {
                     elevate::elevate_if_needed()?;
                     set_programs()?;
                     unsafe {
-                        registry::write_reg(PROGRAMS.get().unwrap(), registry::GpuMode::Integrated)?
-                    };
+                        if use_optimus {
+                            optimus::integrated()?;
+                        } else {
+                            registry::write_reg(
+                                PROGRAMS.get().unwrap(),
+                                registry::GpuMode::Integrated,
+                            )?
+                        }
+                    }
                     println!("Set global GPU to integrated GPU.");
                 }
                 "reset" => {
                     elevate::elevate_if_needed()?;
-                    unsafe { registry::write_reg(&vec![], registry::GpuMode::None)? };
+                    unsafe {
+                        if use_optimus {
+                            optimus::reset()?;
+                        } else {
+                            registry::write_reg(&vec![], registry::GpuMode::None)?
+                        }
+                    };
                     println!("Reset!");
                 }
                 // env!("CARGO_PKG_VERSION") is the cargo version
---
+++ src/optimus.rs
@@ -0,0 +1,10 @@
+use anyhow::Result;
+pub fn integrated() -> Result<()> {
+    todo!()
+}
+pub fn dedicated() -> Result<()> {
+    todo!()
+}
+pub fn reset() -> Result<()> {
+    todo!()
+}
